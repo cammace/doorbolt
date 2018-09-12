1604289d5637134e619ce8dac4243c11f5ab4855
Cameron Mace <cameronmace10@gmail.com>
2018-09-12 14:23:38 -0400

    initial project setup

---
+++ app/src/main/java/com/cammace/doorbolt/StoreListActivity.kt
@@ -0,0 +1,12 @@
+package com.cammace.doorbolt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StoreListActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_storelist)
+  }
+}
